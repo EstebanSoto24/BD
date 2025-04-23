c5f02d88253ceac7b3911d9e62607fe28e9347e0
alumno <estebansotsal@gmail.com>
2025-04-23 12:28:22 +0200

    subiendo ejer6.sql

---
+++ tema7/p2/ejer6.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+USE funciones $$
+DROP PROCEDURE IF EXISTS salario $$
+CREATE PROCEDURE salario (id int, nombre varchar(255), edad int, salario int, s_min int)
+BEGIN
+
+	IF salario > s-min
+		THEN
+			INSERT INTO Empleados VALUES (id, nombre, edad, salario);
+		ELSE
+			SELECT 'Este salario el menor que el salario mínimo' AS 'Mensaje';
+	END IF;
+
+END; $$
+DELIMITER ;
+
